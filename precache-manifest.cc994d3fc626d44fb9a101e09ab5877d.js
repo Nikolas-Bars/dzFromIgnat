b5373b92f1d56a3a48f4ebe58c1c9cc7d2972dac
Nikolas-Bars <kasp2409@mail.ru>
2022-03-03 16:50:23 +0300

    Updates

---
+++ precache-manifest.cc994d3fc626d44fb9a101e09ab5877d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53bcb9d407a6f6fbfd6c93fa5f7680c",
+    "url": "/%3Cproject-repo%3E/index.html"
+  },
+  {
+    "revision": "3171efa31ad89f0fe66d",
+    "url": "/%3Cproject-repo%3E/static/css/main.133f604d.chunk.css"
+  },
+  {
+    "revision": "4f592f4d869903b82b5b",
+    "url": "/%3Cproject-repo%3E/static/js/2.16e84104.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%3Cproject-repo%3E/static/js/2.16e84104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3171efa31ad89f0fe66d",
+    "url": "/%3Cproject-repo%3E/static/js/main.32c8e1b8.chunk.js"
+  },
+  {
+    "revision": "1323fe6851f708a6a7f0",
+    "url": "/%3Cproject-repo%3E/static/js/runtime-main.e24ef637.js"
+  }
+]);
